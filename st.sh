18d38117d511e298e6ce07e6d60aea0460ed89c2
mark-summerfield <mark@qtrac.eu>
2025-07-03 10:00:54 +0100

    m

--- st.sh
+++ st.sh
@@ -3,7 +3,9 @@ nagelfar.sh \
     | grep -v Unknown.command \
     | grep -v Unknown.variable \
     | grep -v Variable.*is.never.read
+echo --- Tests ---
 ./test_store.tcl
+echo -------------
 du -sh .git
 ls -sh .store.str
 clc -s -l tcl

f9344e9517144f0ed699a48f533c18f3d7da93a6
mark-summerfield <mark@qtrac.eu>
2025-07-15 07:46:25 +0100

    m

--- st.sh
+++ st.sh
@@ -2,6 +2,7 @@
 nagelfar.sh \
     | grep -v Unknown.command \
     | grep -v Unknown.variable \
+    | grep -v No.info.on.package.*found \
     | grep -v Bad.option.-striped.to..ttk::treeview. \
     | grep -v Variable.*is.never.read \
     | grep -v Found.constant.*which.is.also.a.variable \

d6743d7bbefbf0cac2c1bf8446d233ebeb5abcce
mark-summerfield <mark@qtrac.eu>
2025-07-21 09:17:03 +0100

    m

--- st.sh
+++ st.sh
@@ -7,7 +7,14 @@ nagelfar.sh \
     | grep -v Variable.*is.never.read \
     | grep -v Found.constant.*which.is.also.a.variable \
     | grep -v Suspicious.variable.name...my.varname \
-    | grep -v test_store.tcl.*Found.constant..filename
+    | grep -v test_store.tcl.*Found.constant..filename \
+    | grep -v Unknown.subcommand..home..to..file. \
+    | grep -v Wrong.number.of.arguments.*to..encoding.convertfrom \
+    | grep -v Wrong.number.of.arguments.*to..ini..open \
+    | grep -v Wrong.number.of.arguments.*to..self \
+    | grep -v .*Argument.*at.*line \
+    | grep -v Non.constant.definition...Db \
+    | grep -v Unknown.subcommand..load..to..FileData
 echo --- Tests ---
 ./test_store.tcl
 echo -------------
